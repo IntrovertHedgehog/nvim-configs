84ed3532a74a7a34595601153bb9406243ec8c0f
IntrovertHedgehog <larby2409@gmail.com>
2023-06-18 14:11:13 +0800

    lsp saga

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -23,13 +23,23 @@ M.nvimtree = {
   },
 }
 
+M.lspconfig = {
+  plugin = true,
+  n = {
+    ["<leader>Ra"] = { "<cmd>Lspsaga rename ++project<CR>", "Rename in Project" },
+    ["<leader>wd"] = { "<cmd>Lspsaga show_workspace_diagnostics<CR>", "Workspace Diagnostics" },
+    ["<leader>K"] = { "<cmd>Lspsaga hover_doc ++keep<CR>", "Persistent Hover" },
+    ["<leader>ca"] = { "<cmd>Lspsaga code_action<CR>", "Code Actions" },
+  },
+}
+
 M.general = {
   n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "Left Pane"},
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "Right Pane"},
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "Lower Pane"},
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "Upper Pane"},
-  }
+    ["<C-h>"] = { "<cmd>TmuxNavigateLeft<CR>", "Left Pane" },
+    ["<C-l>"] = { "<cmd>TmuxNavigateRight<CR>", "Right Pane" },
+    ["<C-j>"] = { "<cmd>TmuxNavigateDown<CR>", "Lower Pane" },
+    ["<C-k>"] = { "<cmd>TmuxNavigateUp<CR>", "Upper Pane" },
+  },
 }
 
 return M

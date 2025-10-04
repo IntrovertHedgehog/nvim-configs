df1b6985b1cb2c1f3e029bc3a28f614dcd310ea0
IntrovertHedgehog <larby2409@gmail.com>
2025-10-04 17:43:49 +0800

    foldlevel

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -18,7 +18,9 @@ vim.api.nvim_create_autocmd("BufRead", {
   callback = function()
     vim.api.nvim_create_autocmd("BufWinEnter", {
       once = true,
-      command = "normal! zx zM zR",
+      callback = function(ev)
+        vim.opt_local.foldlevel = 99
+      end
     })
   end,
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,6 +23,32 @@ local default_plugins = {
     init = function()
       require("core.utils").load_mappings "nvterm"
     end,
+    opts = {
+      terminals = {
+        shell = vim.o.shell,
+        list = {},
+        type_opts = {
+          float = {
+            relative = "editor",
+            row = 0.05,
+            col = 0.05,
+            width = 0.9,
+            height = 0.7,
+            border = "single",
+          },
+          horizontal = { location = "rightbelow", split_ratio = 0.3 },
+          vertical = { location = "rightbelow", split_ratio = 0.5 },
+        },
+      },
+      behavior = {
+        autoclose_on_quit = {
+          enabled = false,
+          confirm = true,
+        },
+        close_on_exit = true,
+        auto_insert = true,
+      },
+    },
     config = function(_, opts)
       require "base46.term"
       require("nvterm").setup(opts)
